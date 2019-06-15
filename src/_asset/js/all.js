7b0ec01079e5dd07e9fe22426b2786249724b9ad
Bogdan Negura <id.bogdann@yahoo.com>
2019-06-15 23:40:49 +0100

    update the loading feature of YoutTube videos

--- src/_asset/js/all.js
+++ src/_asset/js/all.js
@@ -1,15 +1,29 @@
 let iframeList = Array.from(document.querySelectorAll('iframe'));
+
+// load any youtube that hasn't been loaded on initial page scroll
 let handlerFunction = () => {
   //- set the src attribute value to be the value of data-src
-  iframeList.map(iframe => {
+  iframeList
+  // remove the movies that have been already loaded
+  .filter(iframe => !iframe.src)
+  .map(iframe => {
     iframe.src = iframe.dataset.src;
   });
   document.removeEventListener('scroll', handlerFunction);
 };
 let scrollListener = document.addEventListener('scroll', handlerFunction);
+// load any youtube that is in view on page load
+(() => {
+  iframeList.map(iframe => {
+    if (iframe.getBoundingClientRect().top < window.innerHeight) {
+      iframe.src = iframe.dataset.src;
+    }
+  })
+})();
 
+// hide Roberto's email
 let bookSpeakerList = Array.from(document.querySelectorAll('.book-me'));
 bookSpeakerList.map(bookSpeaker => bookSpeaker.addEventListener('click', e => {
   bookSpeaker.href = 'mai' + 'lto:booking' + '@robertoblake.com' + '?Subject=' + 'Bookng%20Speaking' + '%20Session';
 
-}))
+}));
